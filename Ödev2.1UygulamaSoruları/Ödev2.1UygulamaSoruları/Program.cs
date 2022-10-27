bcba6f78289a4ad0ba954c6dc0f12b31530f8154
hilalldavas <hilalldavas@gmail.com>
2022-10-27 13:10:46 +0300

    Odev2.1UygSoru

---
+++ Ödev2.1UygulamaSoruları/Ödev2.1UygulamaSoruları/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ödev2._1UygulamaSoruları
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(48/6/4);
+            Console.WriteLine(24/3*2);
+            Console.WriteLine(2^2+5-1+4);
+            Console.WriteLine((7+4)*2-1+8/2);
+            Console.WriteLine((5-1)*2-1+7^2/2);
+        }
+    }
+}
